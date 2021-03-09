8c0b09e779865a8ee0dc78bd22f3273f0ab518e8
ayanda <64249187+ayandagatsha@users.noreply.github.com>
2021-03-09 14:46:25 +0400

    v1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 
 function App() {
+  const [headingText, setHeading] = React.useState("Hello");
+  function handleClick() {
+    setHeading("Submitted");
+  }
+
+  const [mouseOver, setMouse] = React.useState(false);
+
+  function isMouseOver() {
+    setMouse(true);
+  }
+
+  function isMouseOut() {
+    setMouse(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        onClick={handleClick}
+        onMouseOver={isMouseOver}
+        onMouseOut={isMouseOut}
+        style={{ backgroundColor: mouseOver ? "black" : "white" }}
+      >
+        Submit
+      </button>
     </div>
   );
 }
